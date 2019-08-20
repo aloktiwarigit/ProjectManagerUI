c11ae82b2633b9708c6c0632c33df7efd0e9f4ca
Alok Tiwari <alokkumar.tiwari@cognizant.com>
2019-08-20 13:48:20 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddUserComponent } from './UI/add-user/add-user.component';
+import { AddProjectComponent } from './UI/add-project/add-project.component';
+import { AddTaskComponent } from './UI/add-task/add-task.component';
+import { ViewTaskComponent } from './UI/view-task/view-task.component';
+import { EditTaskComponent } from './UI/edit-task/edit-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/view-task' , pathMatch:'full'},
+  {path:'add-user',component:AddUserComponent},
+  {path:'add-project',component:AddProjectComponent},
+{path:'add-task',component:AddTaskComponent},
+{path:'view-task',component:ViewTaskComponent},
+{path:'edit-task/:taskid',component:EditTaskComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProjectComponent } from './UI/add-project/add-project.component';
+import { AddTaskComponent } from './UI/add-task/add-task.component';
+import { AddUserComponent } from './UI/add-user/add-user.component';
+import { ViewTaskComponent } from './UI/view-task/view-task.component';
+import { EditTaskComponent } from './UI/edit-task/edit-task.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AddProjectComponent,
+    AddTaskComponent,
+    AddUserComponent,
+    ViewTaskComponent,
+    EditTaskComponent,
+    
+    
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
